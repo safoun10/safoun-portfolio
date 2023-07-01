cd85e18bc9a4fd1811a9df482f1ea5e54ff6be84
Safoun <safoun.official@gmail.com>
2023-07-01 12:24:20 +0600

    updating ...

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,4 @@
+import Done from "../done/Done";
 import "./Home.css";
 
 const Home = () => {
@@ -8,42 +9,17 @@ const Home = () => {
 			<div className="block-3"></div>
 			<div>
 				<div
-					className="row mx-auto align-items-center py-3"
+					className="mx-auto align-items-center text-one name-box"
 					style={{ maxWidth: "1200px" }}
 				>
-					<div className="col-7 text-one">
-						<div className="display-2 ">
-							MD Sultan Mahmud Safoun
-						</div>
-						<div className="fs-4 ">Frontend Developer</div>
+					<div className="display-1">
+						MD Sultan Mahmud Safoun
 					</div>
-					<div className="col-5">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Recusandae magnam praesentium fugit ad laudantium
-						aliquid, sunt eaque velit accusantium vero vitae nulla
-						tenetur expedita amet quia! Quae quidem hic sed. Magni
-						ab similique est rem vel blanditiis recusandae, sunt
-						quasi facilis quia ad assumenda sapiente placeat
-						corporis fuga, reiciendis facere minima nemo vero
-						dignissimos perspiciatis modi, laboriosam inventore.
-						Amet aut quidem earum dolorum qui molestias cupiditate,
-						soluta, hic unde facere, cumque fuga quo! Voluptatem
-						expedita rem aut id, fuga nihil. Impedit accusamus
-						itaque, enim officia autem vero qui quaerat sunt laborum
-						eos modi velit labore dignissimos. Fugit eligendi
-						tenetur facere architecto debitis sint, repellat natus
-						in ex perferendis deleniti doloribus molestias obcaecati
-						vel deserunt odio neque iure harum, aperiam aspernatur.
-						Nobis omnis dolorum perspiciatis provident officia
-						consequuntur! Omnis quaerat vel voluptas distinctio
-						eveniet tempora libero, placeat aliquid nostrum
-						inventore temporibus sequi amet error eaque ex dolorem
-						quis iusto. Ex et recusandae officia deserunt blanditiis
-						vitae, impedit atque iusto assumenda exercitationem
-						quaerat alias provident doloremque cupiditate temporibus
-						odio fuga at maxime.
-					</div>
+					<div className="fs-4 ">Frontend Developer</div>
 				</div>
+			</div>
+			<div className="mx-auto pt-5" style={{ maxWidth: "1200px" }}>
+				<Done></Done>
 			</div>
 		</div>
 	);
--- src/pages/body/Body.jsx
+++ src/pages/body/Body.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
-import TopNav from "../../components/topnav/TopNav";
+// import TopNav from "../../components/topnav/TopNav";
+import Footer from "../../components/footer/Footer";
 
 
 const Body = () => {
     return (
         <div>
-            <TopNav></TopNav>
+            {/* <TopNav></TopNav> */}
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
