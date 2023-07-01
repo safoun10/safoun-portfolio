5bb00896cf99395fa51f8ba33118f6f2a642a030
Safoun <safoun.official@gmail.com>
2023-07-01 13:47:02 +0600

    making a new fresh site from beggining

--- src/pages/body/Body.jsx
+++ src/pages/body/Body.jsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
-// import TopNav from "../../components/topnav/TopNav";
 import Footer from "../../components/footer/Footer";
 
-
 const Body = () => {
-    return (
-        <div>
-            {/* <TopNav></TopNav> */}
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+	return (
+		<div>
+			<Outlet></Outlet>
+			<Footer></Footer>
+		</div>
+	);
 };
 
 export default Body;
