535f5bcc3584ab11365def5895c979bbecfc1350
Safoun <safoun.official@gmail.com>
2023-07-03 16:01:29 +0600

    email js added

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		<AnimatedCursor
 			// color="#fff"
 			innerSize={12}
-			outerSize={10}
+			outerSize={8}
 			color="0, 0, 0"
 			outerAlpha={0.3}
 			innerScale={0.7}
@@ -22,15 +22,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 			trailingSpeed={10}
 			clickables={[
 				"a",
-				'input[type="text"]',
-				'input[type="email"]',
-				'input[type="number"]',
-				'input[type="submit"]',
-				'input[type="image"]',
-				'input[type="password"]',
 				"label[for]",
 				"select",
-				"textarea",
 				"button",
 				".link",
 			]}
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,113 @@
-import React from 'react';
-import TopNav from '../../components/topNav/TopNav';
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+import TopNav from "../../components/topNav/TopNav";
+import "./Contact.css";
 
 const Contact = () => {
+	const form = useRef();
 
-    return (
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		const $form = e.target;
+		let name = $form.from_name.value;
+		let email = $form.user_email.value;
+		let message = $form.message.value;
+
+		console.log(name, email, message);
+
+		if (name !== "" || email !== "" || message !== "") {
+			emailjs
+				.sendForm(
+					"service_5ngx1w9",
+					"template_uk28ye3",
+					form.current,
+					"gg9Rc1I9kyo6abP5F"
+				)
+				.then(
+					() => {
+						name = "";
+						email = "";
+						message = "";
+					},
+					(error) => {
+						console.log(error.text);
+					}
+				);
+		} else {
+			console.log("gotcha");
+		}
+	};
+
+	return (
 		<div>
 			<TopNav></TopNav>
+			<div
+				className="mx-auto mt-5 pt-3 text-two"
+				style={{ maxWidth: "1200px" }}
+			>
+				<div className="row mt-2">
+					<div className="col-6 bg-dark p-5 rounded-5 d-flex flex-column gap">
+						<div className="social-card">
+							linkedIn
+						</div>
+						<div className="social-card">
+							linkedIn
+						</div>
+						<div className="social-card">
+							linkedIn
+						</div>
+						<div className="social-card">
+							linkedIn
+						</div>
+						<div className="social-card">
+							linkedIn
+						</div>
+					</div>
+					<div className="col-6">
+						<div className="pb-4 display-6">
+							You Can Send Me an Email
+						</div>
+						<form ref={form} onSubmit={sendEmail}>
+							<div>
+								<label className="fs-5 ps-2">Your Name</label>
+								<input
+									className="inputs fs-5"
+									type="text"
+									name="from_name"
+									required
+								/>
+							</div>
+							<div>
+								<label className="fs-5 ps-2">Your Email</label>
+								<input
+									className="inputs fs-5"
+									type="email"
+									name="user_email"
+									required
+								/>
+							</div>
+							<div>
+								<label className="fs-5 ps-2">
+									Your Message
+								</label>
+								<textarea
+									className="text-area fs-5"
+									name="message"
+									required
+								/>
+							</div>
+							<div className="pt-2">
+								<input
+									className="w-100 bg-dark send-btn fs-5 btn"
+									type="submit"
+									value="Send"
+								/>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
