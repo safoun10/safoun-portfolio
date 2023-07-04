9d455d086ec885e813a91d27dba50ff1b0c89b22
Safoun <safoun.official@gmail.com>
2023-07-04 15:03:07 +0600

    made projects in homepag responsive

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
 			style={{ maxWidth: "1200px" }}
 		>
 			<div className="display-1 ps-4">My Projects</div>
-			<div className="row">
-				<div className="pt-5 col-6">
+			<div className="row w-100 mx-auto">
+				<div className="pt-5 col-lg-6 col-11 mx-auto">
 					<div className="project-img"></div>
 					<Link
 						to={"https://foodie-fiesta-449d3.web.app/home"}
@@ -20,7 +20,7 @@ const Projects = () => {
 						</div>
 					</Link>
 				</div>
-				<div className="pt-5 col-6">
+				<div className="pt-5 col-lg-6 col-11 mx-auto">
 					<div className="project-img-2"></div>
 					<Link
 						to={"https://euphoria-safoun.web.app/"}
@@ -32,10 +32,10 @@ const Projects = () => {
 					</Link>
 				</div>
 			</div>
-			<div className="d-flex justify-content-end">
+			<div className="d-flex justify-content-end mt-4">
 				<Link to={"/projects"}>
-					<div className="btn bg-dark text-white px-5 rounded-3 me-5 py-2 fs-5">
-						More Projects
+					<div className="btn bg-dark text-white px-lg-5 rounded-0 py-2 fs-5 me-4 me-xl-0">
+						<div className="px-5 mx-lg-5">More Projects</div>
 					</div>
 				</Link>
 			</div>
